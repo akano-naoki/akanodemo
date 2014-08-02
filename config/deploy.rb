526222da9c70cd0ec4bc65586ac3d4ff6441dbbf
erosroad <erosroad@yahoo.co.jp>
2014-08-02 22:23:54 +0900

    deploy.rb no こめんとを消して、unicornの設定を追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,23 +15,13 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      task :restart do
-        invoke 'unicorn:restart'
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+    invoke 'unicorn:restart'
   end
 
   after :publishing, :restart
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+
   end
 
 end
