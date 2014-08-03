3a67a7a54774a4f26125dbdceb80a0aaaa37e68e
erosroad <erosroad@yahoo.co.jp>
2014-08-03 10:03:11 +0900

    今の作業に不要なファイルがエラーだしているため一時削除

--- app/models/tweet_mongo.rb
+++
@@ -1,9 +0,0 @@
-class TweetMongo
-  include Mongoid::Document
-  field: :id_str, type: String
-  field: :screen_name, type: String
-  field: :profile_image_url, type: String
-  field: :text, type: String
-  field: :created_at, type: DateTime
-
-end
