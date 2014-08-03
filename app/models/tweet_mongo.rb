eecd287954d8a38ae881412691442e36c4a48a70
erosroad <erosroad@yahoo.co.jp>
2014-08-03 10:01:52 +0900

    typo

--- app/models/tweet_mongo.rb
+++ app/models/tweet_mongo.rb
@@ -1,9 +1,9 @@
 class TweetMongo
   include Mongoid::Document
-  field: :id_str, String
-  field: :screen_name, String
-  field: :profile_image_url, String
-  field: :text, String
-  field: :created_at, DateTime
+  field: :id_str, type: String
+  field: :screen_name, type: String
+  field: :profile_image_url, type: String
+  field: :text, type: String
+  field: :created_at, type: DateTime
 
 end
